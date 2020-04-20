659a65803c743193cf92d320ac98e2258979dde8
Joby Mathew <jobymathew.mec@gmail.com>
2020-04-20 22:22:47 +0530

    added front-end and components

---
+++ core/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.db.models import Q
+from .models import Journal
+
+def is_valid_queryparam(param):
+    return param != '' and param is not None
+
+def BootstrapFilterView(request):
+    qs = Journal.objects.all()
+    title_contains_query = request.GET.get('title_contains')
+    id_exact_query = request.GET.get('id_exact')
+    title_or_author_query = request.GET.get('title_or_author')
+    view_count_min = request.GET.get('view_count_min')
+    view_count_max = request.GET.get('view_count_max')
+
+    if is_valid_queryparam(title_contains_query):
+        qs = qs.filter(title__icontains=title_contains_query)
+    
+    elif is_valid_queryparam(id_exact_query):
+        qs = qs.filter(id=id_exact_query)
+    
+    elif is_valid_queryparam(title_or_author_query):
+        qs = qs.filter(Q(title__icontains=title_or_author_query) | Q(author__name__icontains=title_or_author_query)).distinct()
+    
+    if is_valid_queryparam(view_count_min):
+        qs = qs.filter(views__gte=view_count_min)
+    
+    if is_valid_queryparam(view_count_max):
+        qs = qs.filter(views__lt=view_count_max)
+
+    context = {
+        'queryset' : qs
+    }
+    return render(request,'bootstrap_form.html',context)
